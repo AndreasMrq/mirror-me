419ac46891a6767caa7b19748c24c041b2691634
Andi <andreas_marquardt@outlook.com>
2019-05-22 12:18:39 +0200

    added a gitignore File

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+import flask as fl
 from flask import Flask
+#from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return fl.render_template('hello_world.html',name="Andi")
