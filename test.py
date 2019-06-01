01ff67941432522b52fcb4b60693c02748ebf53d
Andi <andreas_marquardt@outlook.com>
2019-06-01 20:21:21 +0200

    Initialized Weather.py

---
+++ test.py
@@ -0,0 +1,10 @@
+values={"k1":10,"k2":20}
+print(values)
+
+class NEW():
+    def dosmthg(self):
+        self.attr="10"
+
+new=NEW()
+new.dosmthg()
+print(new.attr)
---
+++ weather.py
@@ -0,0 +1,23 @@
+import requests as rq
+
+# Coordinates of Bad Tölz for the URL
+lat = 47.761700
+lon = 11.564570
+#Dark Sky API 
+API="2fe205a368529543659196e1d77daaaf"
+darksky_url=f"https://api.darksky.net/forecast/{API}/{lat},{lon}"
+
+class Weather():
+    def __init__(self):
+        self.request()
+
+    def request(self):
+        r = rq.get(darksky_url)
+        rj=r.json()
+        self.information={}
+        self.information["today"]={"temp":self.cel(rj["currently"]["temperature"]),"icon":rj["currently"]["icon"]}
+        self.information
+
+    def cel(temp):
+        #temp is noch ein String!
+        return (temp-32)*(5/9)
